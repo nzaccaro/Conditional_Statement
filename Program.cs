6039d0c552846f822eca242ac4f68910de35e870
Nick Zaccaro <nzaccaro@mail.usf.edu>
2020-01-28 17:02:52 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+/*
+ Author: Nick Zaccaro
+ Date: 1/28/2020
+ Description: Program that uses the practice of conditional statements after gaining input from the user
+ */
+
+namespace Conditional_Statement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("");
+        }
+    }
+}
